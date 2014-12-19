29a7657ef25c0a675c26dda9451285664bb86111
davidcolman89 <colmanrodrigo@gmail.com>
2014-12-19 12:31:15 -0300

    Modificaciones varias: Agregar: - info detallada del pet shop/veterinario. Globo. - socialmedia de la info detallada (facebook, twitter). - Mascota. Datos de la mascota (recordar q un mismo dueño puede tener mas de una) y vista de la HC (recorda lo del nombre de veterinario).

---
+++ app/Mapa.js
@@ -0,0 +1,82 @@
+var Mapa = {};
+
+Mapa.mapOptions = {};
+
+Mapa.markers = [];
+
+Mapa.divMap = "";
+
+Mapa.map = {};
+
+Mapa.initializeMap = function(options,mapElement){
+    Mapa.mapOptions = options;
+    Mapa.divMap = mapElement;
+    Mapa.map = new google.maps.Map(Mapa.divMap, Mapa.mapOptions);
+}
+
+Mapa.Dibujar = function (places, popUp) {
+    var places = places || null;
+    var n = 1;
+
+    if (places != 0)
+    {
+        var place = new Array();
+        var i;
+
+        for (i in places)
+        {
+            var lat = parseFloat(places[i]['lat']);
+            var lng = parseFloat(places[i]['lng']);
+            place[i] = new google.maps.LatLng(lat, lng);
+        }
+
+        for (i in place)
+        {
+            var marker = Mapa.marker(place[i], places[i]["notas"]);
+
+            if (popUp)
+            {
+                Mapa.addPopUP(marker, places[i]["contenidoPopUp"])
+            }
+        }
+    }
+};
+
+Mapa.addMarker = function (coordenadas, callback) {
+    var lng = parseFloat(coordenadas.lng);
+    var lat = parseFloat(coordenadas.lat);
+    var googleMapsLatLng = new google.maps.LatLng(lat, lng);
+    var marker = Mapa.marker(googleMapsLatLng);
+
+    Mapa.markers.push(marker);
+    Mapa.map.setZoom(15);
+    Mapa.map.setCenter(googleMapsLatLng);
+
+    if(!empty(callback)) callback(Mapa.map, googleMapsLatLng, marker);
+
+};
+
+Mapa.marker = function (googleMapsLatLng, titulo, nota, icono) {
+    return new google.maps.Marker({
+        position: googleMapsLatLng,
+        map: Mapa.map,
+        title: titulo || '',
+        note: nota || null,
+        icon: icono || null
+    });
+};
+
+Mapa.addPopUP = function (marker, content) {
+    var infowindow = new google.maps.InfoWindow({
+        content: content
+    });
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.open(Mapa.map, marker);
+    });
+};
+
+Mapa.clearMarkers = function () {
+    for (var i = 0; i < Mapa.markers.length; i++) {
+        Mapa.markers[i].setMap(null);
+    }
+};
